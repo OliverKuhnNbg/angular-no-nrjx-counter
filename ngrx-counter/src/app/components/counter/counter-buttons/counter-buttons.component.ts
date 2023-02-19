b43eedfd2a405899341b4f83ec50d4bf38a139c8
Oliver <oliver.kuhn@ancud.de>
2023-02-19 19:08:08 +0100

    copy repository - angular-nrjx-counter

---
+++ ngrx-counter/src/app/components/counter/counter-buttons/counter-buttons.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-buttons',
+  templateUrl: './counter-buttons.component.html',
+  styleUrls: ['./counter-buttons.component.scss']
+})
+export class CounterButtonsComponent implements OnInit{
+  @Output() increment = new EventEmitter<void>();
+  @Output() decrement = new EventEmitter<void>();
+  @Output() reset = new EventEmitter<void>();
+
+  constructor() {}
+
+  ngOnInit(): void {
+      
+  }
+
+  onIncrement() {
+    this.increment.emit();
+  }
+  onDecrement() {
+    this.decrement.emit();
+  }
+  onReset() {
+    this.reset.emit();
+  }
+}
